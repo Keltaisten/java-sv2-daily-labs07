547ed216d9dbdf1035c3517f4cbfaca9ffb4e4e1
Keltaisten <keltaisten@gmail.com>
2021-11-23 23:06:24 +0100

    Movie

---
+++ week07-lab-tasks/src/main/java/day02/MovieService.java
@@ -0,0 +1,17 @@
+package day02;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieService {
+    List<Movie> movieList = new ArrayList<>();
+
+    public MovieService(Path path) {
+//        this.movieList = movieList;
+
+//        try {
+//
+//        }
+    }
+}
